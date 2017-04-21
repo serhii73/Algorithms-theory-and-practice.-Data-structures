be96f30fddff1a636045780198c551d1255b20b3
Sergey <sabarovsky@yahoo.com>
2017-04-21 22:33:49 +0300

    update 1

--- The underlying data structures/1.py
+++ The underlying data structures/1.py
@@ -1,29 +1,45 @@
-def is_balanced(br):
-    n = 0
+def isBalanced(br):
     brackets = []
+    stack = []
     for i in br:
         if i == '{' or i == '}' or i == '[' or i == ']' or i == '(' or i == ')':
             brackets.append(i)
-    stack = []
+
+    len_our_brackets = [i for i in range(len(brackets))]
     d = {}
-    d['{'] = '}'
-    d['('] = ')'
-    d['['] = ']'
-    if brackets[0] == ']' or brackets[0] == ')' or brackets[0] == '}' or len(brackets) % 2 != 0:
+    for bracket, len_our_bracket in zip(brackets, len_our_brackets):
+        d.setdefault(bracket, []).append(len_our_bracket)
+    if brackets[0] == ']' or brackets[0] == ')' or brackets[0] == '}':
         return 1
     else:
-        stack.append(brackets[0])
-        for bracket in brackets[1:]:
-            try:
-                n+=1
-                if bracket == d[stack[-1]]:
-                    stack.remove(stack[-1])
-            except IndexError:
-                n+=1
-        if len(stack) == 0:
-            return 'Success'
-        else:
-            return n
+        for our_bracket in brackets:
+            if our_bracket == '{' or our_bracket == '[' or our_bracket == '(':
+                stack.append(our_bracket)
+            else:
+                if len(stack) == 0:
+                    return len(brackets)
+                try:
+                    if stack[-1] == '{' and our_bracket == '}' or stack[-1] == '(' \
+                            and our_bracket == ')' or stack[-1] == '[' and our_bracket == ']':
+                        stack.pop()
+                    else:
+                        stack.append(our_bracket)
+                except:
+                    pass
 
-br = input()
-print(is_balanced(br))
+    # print(stack)
+    # print(d)
+    if len(stack) == 0:
+        return "Success"
+    else:
+        for i in stack:
+            # print('i: ', i)
+            if i == '}' or i == ')' or i == ']':
+                return d[i][-1]+1
+            else:
+                return len(brackets)
+
+
+b = input()
+a = isBalanced(b)
+print(a)
