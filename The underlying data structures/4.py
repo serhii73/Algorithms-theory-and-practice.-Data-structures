82d767400af2c324dbd840f58d4a0e35a4b19d85
Sergey <sabarovsky@yahoo.com>
2017-04-25 09:20:10 +0300

    ready 4!

--- The underlying data structures/4.py
+++ The underlying data structures/4.py
@@ -1,49 +1,31 @@
 stack = []
-
+stack_max = []
 
 def push(v):
     stack.append(int(v))
 
 def pop():
     stack.pop()
-
-def maximum(stack):
-    global big
-    big = max(stack)
-    print(big)
-
-def prBig():
-    print(big)
+    stack_max.pop()
 
 q = int(input())
-# q = 10
 task = []
 for i in range(q):
     z = input()
     task.append(z)
-# task = ['push 1', 'push 1', 'push 1', "max", 'push 1', 'push 2', 'max', 'pop', 'push 1', 'max', 'max', 'max']
-
-try:
-    for idx, i in enumerate(task):
-        if i == 'max' and task[idx+1] == 'max':
-            i = 'ptintBig'
-except IndexError:
-    pass
 
 
 for a in task:
     if a.split()[0] == "push":
-            push(a.split()[1])
-    elif a == 'printBig':
-        prBig()
+        push(a.split()[1])
+        try:
+            if int(stack_max[-1]) <= int(a.split()[1]):
+                stack_max.append(int(a.split()[1]))
+            else:
+                stack_max.append(stack_max[-1])
+        except IndexError:
+            stack_max.append(stack[-1])
     elif a == "pop":
         pop()
     else:
-        maximum(stack)
-
-
-max_i = []
-for idx, i in enumerate(task):
-    if i == 'max':
-        max_i.append(idx)
-
+        print(stack_max[-1])
