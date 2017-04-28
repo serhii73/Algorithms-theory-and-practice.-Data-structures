1c66979207a65cbab8a05c668631b345c13175e4
Sergey <sabarovsky@yahoo.com>
2017-04-28 20:18:54 +0300

    ready 5

--- The underlying data structures/5.py
+++ The underlying data structures/5.py
@@ -1,19 +1,28 @@
 count = int(input())
 numbers = list(map(int, input().split()))
 step = int(input())
-rezult = []
-stack = []
+
+lr = []
+rl = [0] * count
+max_val = []
+
+k = count-step
+for i in range(count):
+    if i%step == 0:
+        lr.append(numbers[i])
+    else:
+        lr.append(max(lr[i-1], numbers[i]))
 
 
-for i in numbers[:step]:
-    stack.append(i)
+for i in range(count-1, -1, -1):
+    if i == count-1:
+        rl[i] = numbers[i]
+    elif (i+1) % step == 0:
+        rl[i] = numbers[i]
+    else:
+        rl[i] = (max(rl[i+1], numbers[i]))
 
+for i in range(k+1):
+    max_val.append(max(rl[i], lr[i+step-1]))
 
-rezult.append(max(stack))
-
-for i in numbers[step:]:
-    stack.append(i)
-    stack.pop(0)
-    rezult.append(max(stack))
-
-print(*rezult)
+print(*max_val)
